bd5106766c0c550141ea370594d9cec751aa4d26
athulya116 <116333075+athulya116@users.noreply.github.com>
2024-04-01 10:26:27 +0530

    Add files via upload

---
+++ contracts/PropertyOwners.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.2;
+
+contract PropertyOwners {
+
+    mapping (uint => address) properties;
+    uint nbOfProperties = 0;
+
+    function PropertyOwners() {
+    }
+
+    function getContractAddress() public constant returns (address) {
+        return address(this);
+    }
+
+    function AddProperty() {
+        properties[nbOfProperties] = msg.sender;
+        nbOfProperties = nbOfProperties+1;
+    }
+
+    function ChangeOwner(uint propId, address newOwner) {
+        if(tx.origin == properties[propId]) {
+            properties[propId] = newOwner;
+        }
+    }
+
+    function GetOwner(uint propId) public constant returns (address) {
+      return properties[propId];
+    }
+
+    function GetNumberOfProperties() public constant returns (uint) {
+      return nbOfProperties;
+    }
+
+}
